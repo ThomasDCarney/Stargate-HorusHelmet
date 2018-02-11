14797805e0e04b90064ae22e35a5514fda162724
Thomas Carney <30845756+ThomasDCarney@users.noreply.github.com>
2018-02-10 23:08:41 -0600

    Moved mood button function to an ISR and added pulse animation

--- EyeEffects/EyeEffects.ino
+++ EyeEffects/EyeEffects.ino
@@ -2,7 +2,10 @@
  * This is handled by two LED's, white for normal running and red for angry or 
  * attack mode.  
  *
- * No other frills in this version, simplest worst case backup. 
+ * Changing moods/modes is handled via an ISR and each has a unique set of 
+ * values to control their animations.
+ * 
+ * Debouncing is handled in software.
  */
 
 // Define which pins will be used.
@@ -12,77 +15,185 @@ const int WHITE_PIN = 6;
 
 // Used in the software debounce on a momentary switch, tweak as needed. 
 const int DEBOUNCE_LENGTH = 250;
-volatile long oldTime;
+volatile long oldDebounceTime = 0;
 
 // Keeps tab on the mood/mode.
-volatile int isAngry;
+volatile int isAngry = false;
 
 // Illumination levels for both LED's.
-volatile int redLevel;
-volatile int whiteLevel;
+const int maxRedLevel = 175;
+const int minRedLevel = 40;
+int redPeriod = 15;
+volatile int redLevel = maxRedLevel;
 
+const int maxWhiteLevel = 175;
+const int minWhiteLevel = 40;
+volatile int whiteLevel = maxWhiteLevel;
+int whitePeriod = 15;
+
+// Used by animation sequences.
+boolean dimming = true;
+long oldRedTime = 0;    // Not sure we need unique times for red and white
+long oldWhiteTime = 0;  // but doesn't hurt so keeping it.
+
+
+/**
+ * Initial one-time setup.
+ */
 void setup() {
 
+  // Init the pins
   pinMode(BUTTON_PIN, INPUT);
   pinMode(RED_PIN, OUTPUT);
   pinMode(WHITE_PIN, OUTPUT);
   
   // Default mood/mode is normal running.
-  analogWrite(WHITE_PIN, 25);
-  analogWrite(RED_PIN, 0);
-  isAngry = false;
-
-  oldTime = 0;
+  analogWrite(WHITE_PIN, maxWhiteLevel);
+  analogWrite(RED_PIN, 0); // Is off to start.
   attachInterrupt(digitalPinToInterrupt(2), moodButtonISR, RISING);
 
 } // end setup
 
 
+/**
+ * Main loop, all it does is tells a particular LED (depending on mood) to move along 
+ * in it's animation. 
+ */
 void loop() {
 
-  // Main loop is empty.
+  if(isAngry) {
+
+    changeRedLevel();
+    
+  } else {
+
+    changeWhiteLevel();
+    
+  }
   
 } // end loop
 
 
+/**
+ * This method determines the animation used for the red, "angry" LED.
+ */
+void changeRedLevel() {
+
+  long currentTime = millis();
+
+  // Change redPeriod to make the pulse faster/slower.
+  if(currentTime - oldRedTime > redPeriod) {
+
+    // This animation is a simple pulse that dims and brightens.
+    if(dimming) {
+
+      analogWrite(RED_PIN, --redLevel);
+    
+    }  else {
+
+      analogWrite(RED_PIN, ++redLevel);
+      
+    }
+
+    if(redLevel >= maxRedLevel || redLevel <= minRedLevel) {
+
+      dimming = !dimming;
+      
+    }
+
+    oldRedTime = currentTime;
+  
+  }
+  
+} // end changeRedLevel
+
+
+/**
+ * This method determines the animation used for the white, "calm" LED.
+ */
+void changeWhiteLevel() {
+
+  long currentTime = millis();
+
+  // Change whitePeriod to make the pulse faster/slower.
+  if(currentTime - oldWhiteTime > whitePeriod) {
+
+    // This animation is a simple pulse that dims and brightens.
+    if(dimming) {
+
+      analogWrite(WHITE_PIN, --whiteLevel);
+    
+    }  else {
+
+      analogWrite(WHITE_PIN, ++whiteLevel);
+      
+    }
+
+    if(whiteLevel >= maxWhiteLevel || whiteLevel <= minWhiteLevel) {
+
+      dimming = !dimming;
+      
+    }
+
+    oldWhiteTime = currentTime;
+  
+  }
+  
+} // end changeWhiteLevels
+
+
+/**
+ * Interrupt Service Request for switching between angry/calm mood/mode
+ */
 void moodButtonISR() {
 
-    // We know the button "was" pressed and ISR triggered but we want to ignore switch bounce.
+    // Since we're using software debouncing, wasButtonPressed() will tell the ISR whether 
+    // or not to perform its routine.
     if(wasButtonPressed()) {
 
+      // The button is used to change modes.
       isAngry = !isAngry;
-    
+
+      // Init starting values for each state (starting on the high end).
       if(isAngry) {
-      
-        analogWrite(RED_PIN, 100);
+
+        redLevel = maxRedLevel;
+        analogWrite(RED_PIN, redLevel);
         analogWrite(WHITE_PIN, 0);
     
       } else {
-      
+
+        whiteLevel = maxWhiteLevel;
         analogWrite(RED_PIN, 0);
-        analogWrite(WHITE_PIN, 25);
+        analogWrite(WHITE_PIN, whiteLevel);
     
       }
+
+      // Since we're starting high, we should be dimming.
+      dimming = true;
     
   }
   
 } // end moodButtonISR
 
 
+/**
+ * A debounce routine used for the mood switch.
+ */
 boolean wasButtonPressed() {
 
   long currentTime = millis();
   boolean buttonPressed = false;
 
   // Only register the initial button press, assume the bounces die off after
-  // a certain period... DEBOUNCE_LENGTH.
-  if(currentTime - oldTime > DEBOUNCE_LENGTH){
+  // a certain period... tweak DEBOUNCE_LENGTH if needed.
+  if(currentTime - oldDebounceTime > DEBOUNCE_LENGTH){
     
     if(digitalRead(BUTTON_PIN) == HIGH) {
 
       // Record the button was pressed and when.
       buttonPressed = true;
-      oldTime = currentTime;
+      oldDebounceTime = currentTime;
   
     }
   
--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Startgate-HorusHelmet
 Design and code for a replica Horus Guard mask from the movie Stargate.
 
-## V1
+## v1.0
 The simplest option, two LED's (mounted behind the eyes) used to indicate the
 mood/mode of the character/gear. A white LED to indicate standard mode and a Red
 LED for an angry or attack mode. This is more of an initial, fallback setup but
 simple to get up and running.
+
+## v2.0
+Updated the sketch to handle button input via an Interrupt Service Routine
+(ISR). I also added a pulsing animation for both LED's.
