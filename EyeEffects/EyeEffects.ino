ef7d1f037ad03804d57cb68966312846c6d1f02f
Thomas Carney <30845756+ThomasDCarney@users.noreply.github.com>
2018-02-14 14:51:27 -0600

    Added eye animation for the passive state.

--- EyeEffects/EyeEffects.ino
+++ EyeEffects/EyeEffects.ino
@@ -21,12 +21,18 @@ const int DEBOUNCE_LENGTH = 250; // Tweak for the specific switch used.
 volatile long oldDebounceTime = 0; // Keep volatile (modified by ISR)
 
 // Keeps track of mood/mode (angry or passive).
-volatile int isAngry = false; // Keep volatile (modified by ISR).
+volatile boolean isAngry = false; // Keep volatile (modified by ISR).
+volatile boolean moodChanged = false; // Keep volatile (modified by ISR).
 
 // Illumination values for individual colors, 0 = off, 255 = max.
-int red = 200;
-int green = 25;
-int blue = 0;
+int red = 50;
+int green = 50;
+int blue = 75;
+
+// Timing variables for eye animations.
+long lastAngryTime = 0;
+int angrySettlingPeriod = 50;
+int angryStandardPeriod = 100;
 
 /* The NeoPixel Strip object is used to represent/control the pixels. 
  * Parameter 1 = Number of pixels in the strip.
@@ -47,6 +53,7 @@ void setup() {
 
   // Init the pixel strip.
   strip.begin();
+  strip.setPixelColor(0, red, green, blue);
   strip.show(); // no values specified yet so inits all pixels to off.
 
 } // end setup
@@ -58,34 +65,166 @@ void setup() {
  */
 void loop() {
 
-  updateColorValues();
-  strip.setPixelColor(0, red, green, blue);
-  strip.show();
-  delay(50);
+  if(updateColorValues()) {
+
+    strip.setPixelColor(0, red, green, blue);
+    strip.show();
+  
+  }
+  
   
 } // end loop
 
 
 /**
- * This method determines the animation used for pixels in "angry" mode.
+ * This method will handle updating pixel colors.
+ * 
+ * @return - A boolean true if colors were updated, false otherwise.
  */
-void updateColorValues() {
+boolean updateColorValues() {
 
   if(isAngry) {
 
     red = 200;
     green = 25;
     blue = 0;
+
+    return true;
       
   } else {
 
-    red = 100;
-    green = 100;
-    blue = 100;
+    return updatePassiveColors();
     
   }
   
 } // end updateColorValues
+
+
+/**
+ * This method is used to update colors while in the passive state.
+ */
+boolean updatePassiveColors() {
+
+  const int midRed = 115;
+  const int midGreen = 115;
+  const int midBlue = 115;
+  const int colorRange = 50;
+  const int overload = 255 - midRed;
+  const long passiveFlashingPeriod = 4;
+  const long passiveCoolDownPeriod = 6;
+  const long passiveStandardPeriod = 50;
+
+  // Specific to this animation cycle but need to stick around.
+  static long previousPassiveTime = 0;
+  static boolean eyesOverloading = false;
+  static boolean eyesCoolingDown = false;
+  static boolean dimming = false;
+
+  boolean valuesChanged = false;
+  
+  long currentTime = millis();
+
+  /* This outer IF-Else block work kind of like Keyframes or "phases".
+   * Phase 1: Reset the eyes to desired shade.
+   * Phase 2: Overflash like Goa'uld eyes.
+   * Phase 3: Settle back to normal high levels.
+   * Phase 4: Standard slow pulse.
+   */
+  if(moodChanged) { // Phase 1 (based on ISR).
+
+    red = midRed;
+    green = midGreen;
+    blue = midBlue;
+    
+    eyesOverloading = true; // Trigger the next phase.
+    moodChanged = false; // Acknowledge the ISR did it's thing.
+    valuesChanged = true;
+    
+  } else if(eyesOverloading) { // Phase 2.
+
+    // Control animation speed without blocking.
+    if(currentTime - previousPassiveTime >= passiveFlashingPeriod) {
+
+      red++;
+      green++;
+      blue++;
+
+      previousPassiveTime = currentTime;
+      valuesChanged = true;
+      
+    }
+
+    // Once settled, move to standard animation.
+    if(red == midRed + overload) {
+
+      eyesOverloading = false; // Trigger the next phase.
+      eyesCoolingDown = true;
+      
+    }
+
+  } else if(eyesCoolingDown) { // Phase 3.
+
+    // Control animation speed without blocking.
+    if(currentTime - previousPassiveTime >= passiveCoolDownPeriod) {
+
+      red--;
+      green--;
+      blue--;
+
+      previousPassiveTime = currentTime;
+      valuesChanged = true;
+      
+    }
+
+    // Once settled, move to standard animation.
+    if(red == midRed) {
+
+      eyesCoolingDown = false; // Trigger the next phase.
+      dimming = false;
+      
+    }
+
+  } else { // Phase 4.
+
+    // Standard animation after initial flash occured.
+    if(currentTime - previousPassiveTime >= passiveStandardPeriod) {
+      
+      if(dimming) {
+
+        red--;
+        green--;
+        blue--;
+
+        if(red <= midRed - colorRange) {
+
+          dimming = false;
+          
+        }
+        
+      } else {
+
+        red++;
+        green++;
+        blue++;
+
+        if(red >= midRed + colorRange) {
+
+          dimming = true;
+          
+        }
+        
+      }
+
+      previousPassiveTime = currentTime;
+      valuesChanged = true;
+      
+    }
+    
+  }
+
+  return valuesChanged;
+  
+} // end animatePassive
 
 
 /**
@@ -99,8 +238,9 @@ void moodButtonISR() {
 
       // The button is used to change modes.
       isAngry = !isAngry;
+      moodChanged = true;
     
-    }
+    } 
   
 } // end moodButtonISR
 
